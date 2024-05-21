51c9f8c45fcbf709c02ca288e61526869a91b3a6
pranjul <pranjulmishraknp@gmail.com>
2024-05-21 11:23:13 +0530

    Installed external packages.
    > Installed external packages using npm.
    > package-lock.json file was created.
    > Colors.js file to test out the colors module, installed using npm.

---
+++ colors.js
@@ -0,0 +1,6 @@
+const colors = require('colors');   // Using node 'colors' module for testing.
+
+console.log("Test");
+console.log("Test1".red);
+console.log("Test2".bgWhite);
+console.log("Test3".black);
